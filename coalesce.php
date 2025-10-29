e1520631592999a2c8b85dc26226b6fae496c87b
drylikov <denis.rylikov@protonmail.com>
2025-10-29 12:49:04 +0300

    coalesce
    Null Coalesce function for PHP (waiting for the `??` operator...) .

---
+++ coalesce.php
@@ -0,0 +1,51 @@
+<?php
+
+if (!function_exists('coalesce')) {
+
+    /**
+     * @return The first non null argument (or null)
+     */
+    function coalesce() {
+        $notNullArgs = array_filter(func_get_args(), function ($value) {
+            return !is_null($value);
+        });
+        return array_shift($notNullArgs);
+    }
+}
+
+if (!function_exists('coalescy')) {
+
+    /**
+     * @return The first non "falsy" argument (or null)
+     */
+    function coalescy() {
+        $notFalsyArgs = array_filter(func_get_args());
+        return array_shift($notFalsyArgs);
+    }
+}
+
+if (!function_exists('val')) {
+
+    /**
+     * @param array $arr
+     * @param string|int $index
+     * @return The array element or null if it's undefined
+     */
+    function val($arr, $index) {
+        return isset($arr[$index]) ? $arr[$index] : null;
+    }
+}
+
+if (!function_exists('prop')) {
+
+    /**
+     * @param Object $obj
+     * @param string $propertyName
+     * @return The object property or null if it's undefined
+     */
+    function prop($obj, $propertyName) {
+        return isset($obj->$propertyName) ? $obj->$propertyName : null;
+    }
+}
+
+?>
